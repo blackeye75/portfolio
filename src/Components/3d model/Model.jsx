d539966bf89c4d5e7d8475d576a88431f3d35fcd
blackeye75 <priyanshuraj22275@gmail.com>
2024-08-31 23:14:03 +0530

    not fixed

---
+++ src/Components/3d model/Model.jsx
@@ -0,0 +1,39 @@
+import { Canvas } from "@react-three/fiber";
+import "./style.css";
+import { OrbitControls } from "@react-three/drei";
+
+import { Scene } from "./Scene";
+import {
+  Bloom,
+  EffectComposer,
+  ToneMapping,
+} from "@react-three/postprocessing";
+function Model() {
+  return (
+    <>
+      <Canvas flat camera={{ fov: 35 }}>
+        <OrbitControls
+          // enableZoom={false} // Keep zoom enabled
+          // minDistance={2} // Set the minimum zoom distance
+          // maxDistance={} // Set the maximum zoom distance
+        />
+        <ambientLight />
+        <Scene />
+        <EffectComposer>
+          <Bloom
+            intensity={40.0} // The bloom intensity.
+            luminanceThreshold={0.1} // luminance threshold. Raise this value to mask out darker elements in the scene.
+            luminanceSmoothing={1} // smoothness of the luminance threshold. Range is [0, 1]
+            mipmapBlur
+          />
+          {/* <ToneMapping adaptive /> */}
+        </EffectComposer>
+      </Canvas>
+      {/* <h1 className=" text font-bold text-9xl text-center ">
+        Protfolio component
+      </h1> */}
+    </>
+  );
+}
+
+export default Model;
