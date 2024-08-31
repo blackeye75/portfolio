d17867747ffecf68848a7752fb38b1375dba09ab
blackeye75 <priyanshuraj22275@gmail.com>
2024-08-31 18:35:40 +0530

    initiated

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Footer from './Components/footer/Footer'
+import { Header } from './Components/header/Header'
+
+function App() {
+
+  return (
+    <div className='bg-[#121212] min-h-screen w-full text-white' >
+    <Header/>
+    <Footer/>
+    </div>
+  )
+}
+
+export default App
