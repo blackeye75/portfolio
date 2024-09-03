d90028bded3f09c70e4cd3af2a5b7414643f3c69
blackeye75 <priyanshuraj22275@gmail.com>
2024-09-03 14:42:34 +0530

    bug

--- src/Components/Marque.jsx
+++ src/Components/Marque.jsx
@@ -65,7 +65,7 @@ const Marque = () => {
           transition={{ ease: "linear", repeat: Infinity, duration: 10 }}
           src="https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt=""
-          className="w-[35vw] h-[20vw] rounded-lg px-9"
+          className="w-[35vw] h-[20vw] rounded-lg px-10"
         />
         <motion.img
           initial={{ x: 0 }}
